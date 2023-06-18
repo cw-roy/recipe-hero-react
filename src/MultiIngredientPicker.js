3990f7c040851e5a731d8e1b4b1758913630c4de
cw-roy <curtis_roy@msn.com>
2023-06-18 13:52:02 -0700

    Modify add/remove buttons

--- src/MultiIngredientPicker.js
+++ src/MultiIngredientPicker.js
@@ -1,6 +1,8 @@
-// Test version of MultiIngredientPicker.js
+// MultiIngredientPicker.js
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMinusCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const MultiIngredientPicker = ({ ingredients, selectedIngredients, onAddIngredient, onRemoveIngredient }) => {
   const handleAddClick = (ingredient) => {
@@ -17,8 +19,12 @@ const MultiIngredientPicker = ({ ingredients, selectedIngredients, onAddIngredie
       {ingredients.map((ingredient, index) => (
         <div key={index}>
           <span>{ingredient}</span>
-          <button onClick={() => handleAddClick(ingredient)}>Add</button>
-          <button onClick={() => handleRemoveClick(ingredient)}>Remove</button>
+          <button onClick={() => handleAddClick(ingredient)}>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+          <button onClick={() => handleRemoveClick(ingredient)}>
+            <FontAwesomeIcon icon={faMinusCircle} />
+          </button>
         </div>
       ))}
     </div>
@@ -26,3 +32,33 @@ const MultiIngredientPicker = ({ ingredients, selectedIngredients, onAddIngredie
 };
 
 export default MultiIngredientPicker;
+
+
+// Previous version of MultiIngredientPicker.js
+
+// import React from 'react';
+
+// const MultiIngredientPicker = ({ ingredients, selectedIngredients, onAddIngredient, onRemoveIngredient }) => {
+//   const handleAddClick = (ingredient) => {
+//     onAddIngredient(ingredient);
+//   };
+
+//   const handleRemoveClick = (ingredient) => {
+//     onRemoveIngredient(ingredient);
+//   };
+
+//   return (
+//     <div className="multi-ingredient-picker">
+//       <label>Select ingredients:</label>
+//       {ingredients.map((ingredient, index) => (
+//         <div key={index}>
+//           <span>{ingredient}</span>
+//           <button onClick={() => handleAddClick(ingredient)}>Add</button>
+//           <button onClick={() => handleRemoveClick(ingredient)}>Remove</button>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default MultiIngredientPicker;
